e6c80d04596353e454d3a247b65c204765e0616a
lakshmanaram <lakshmanaram.n@gmail.com>
2016-06-11 05:57:12 +0530

    adds Main Activity layout

--- app/src/main/java/com/example/adarsh/notificationapplication/MainActivity.java
+++ app/src/main/java/com/example/adarsh/notificationapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
@@ -14,16 +15,80 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    static boolean cautious_mode = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        ImageButton profile = (ImageButton) findViewById(R.id.myProfile);
+        assert profile!=null;
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent to profile
+            }
+        });
+        Button pid = (Button) findViewById(R.id.pid);
+        assert pid != null;
+        pid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // intent to people in danger
+            }
+        });
+        Button track = (Button) findViewById(R.id.track);
+        assert track != null;
+        track.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to track activity
+            }
+        });
+        final Button cautious = (Button) findViewById(R.id.cautious);
+        assert cautious != null;
+        final Button stopc = (Button) findViewById(R.id.stopcautious);
+        assert stopc != null;
+        if(cautious.getDrawingCacheBackgroundColor() == Color.parseColor("#aaaaaa"))
+            cautious_mode = true;
+        cautious.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!cautious_mode) {
+                    cautious_mode = true;
+                    cautious.setBackgroundColor(Color.parseColor("#aaaaaa"));
+                    cautious.setClickable(false);
+                    stopc.setVisibility(View.VISIBLE);
+                    // call to alarm manager
+                    Toast.makeText(getApplicationContext(), "Activatedd Cautious mode", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        stopc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cautious_mode = false;
+                stopc.setVisibility(View.GONE);
+                cautious.setBackgroundColor(Color.parseColor("#000000"));
+                cautious.setClickable(true);
+                // Calls to cancel Alarm manager
+            }
+        });
+        Button emergency = (Button) findViewById(R.id.emergency);
+        assert emergency != null;
+        emergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // API calls to show emergency
+                // Calls to Alarm manager.
+                // Do the same as in cautious mode.
+            }
+        });
     }
 /*
     @Override
