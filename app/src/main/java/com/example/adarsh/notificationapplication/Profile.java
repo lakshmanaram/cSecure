6aaa85ca30a584411d7d8ba163eeb73194d9fdb4
lakshmanaram <lakshmanaram.n@gmail.com>
2016-06-11 13:42:13 +0530

    adds User class

--- app/src/main/java/com/example/adarsh/notificationapplication/Profile.java
+++ app/src/main/java/com/example/adarsh/notificationapplication/Profile.java
@@ -2,6 +2,10 @@ package com.example.adarsh.notificationapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        //TODO: Receive JSON and update the received_user object
+        User received_user = new User("laksh","lakshmanaram","A102345678","+919000000000","b15 sri harsh19","Adarsh Srivatsan Harshitha");
+        TextView uname = (TextView)findViewById(R.id.profileusername);
+        assert uname!=null;
+        uname.setText(received_user.username);
+        TextView name = (TextView)findViewById(R.id.profilename);
+        assert name!=null;
+        name.setText(received_user.name);
+        TextView aadharno = (TextView)findViewById(R.id.profileaadharno);
+        assert aadharno!=null;
+        aadharno.setText(received_user.aadharno);
+        TextView phone = (TextView)findViewById(R.id.profilephone);
+        assert phone!=null;
+        phone.setText(received_user.phone);
+        //TODO: list
     }
 }
---
+++ app/src/main/java/com/example/adarsh/notificationapplication/User.java
@@ -0,0 +1,16 @@
+package com.example.adarsh.notificationapplication;
+
+/**
+ * Created by lakshmanaram on 11/6/16.
+ */
+public class User {
+    static String username,name,aadharno,phone,contactsid,contactsname;
+    User(String uname,String n,String an,String p,String cid, String cname){
+        username = uname;
+        name = n;
+        aadharno = an;
+        phone = p;
+        contactsid = cid;
+        contactsname = cname;
+    }
+}
